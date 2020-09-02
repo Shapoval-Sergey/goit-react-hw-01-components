999ca4504d28b6a5f86c30d1c3eb853c2d4dc41d
Sergey <shapa92sergey@gmail.com>
2020-09-02 21:15:15 +0300

    Create TransactionHistory component

--- src/components/App.js
+++ src/components/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Profile from "./Profile/Profile";
 import Statistics from "./Statistics/Statistics";
 import FriendList from "./FriendList/FriendList";
+import TransactionHistory from "./TransactionHistory/TransactionHistory";
 import stats from "../statistical-data.json";
 import user from "../user.json";
 import friends from "../friends.json";
+import transactions from "../transactions.json";
 
 function App() {
   return (
@@ -12,6 +14,7 @@ function App() {
       <Profile person={user} />
       <Statistics stats={stats} />
       <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </>
   );
 }
