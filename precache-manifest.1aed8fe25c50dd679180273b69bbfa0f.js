e1cd7dcdbf36ad878939801398815cf1e3d60417
Sergey <shapa92sergey@gmail.com>
2020-09-02 21:44:27 +0300

    Updates

---
+++ precache-manifest.1aed8fe25c50dd679180273b69bbfa0f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aaf085c1c9080749d167e048a9fcbd57",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "cdb66ba5299c0877c581",
+    "url": "/goit-react-hw-01-components/static/css/main.ca207a87.chunk.css"
+  },
+  {
+    "revision": "8d9dc58c1a4c85358da3",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.3cf5bb85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cdb66ba5299c0877c581",
+    "url": "/goit-react-hw-01-components/static/js/main.8ef1d203.chunk.js"
+  },
+  {
+    "revision": "7c1abe1df921e466788f",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.e943b22f.js"
+  }
+]);
